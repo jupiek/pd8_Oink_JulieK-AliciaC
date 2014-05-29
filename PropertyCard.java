b575e6d05c01e198178da8efc27d4a10c34cc93a
jupiekim <jh.julie.kim@gmail.com>
2014-05-29 13:47:41 -0400

    Update PropertyCard.java

--- PropertyCard.java
+++ PropertyCard.java
@@ -8,4 +8,8 @@ public class PropertyCard extends Card {
     	super();
     }
 
+    public String getDescription() {
+	return "Rent for 1: " + rent1 + "\n" + "Rent for 2: " + rent2 + "\n" + "Rent for 3: " + rent3;
+    }
+ 
 }
