c16ae8aa95aab6a6ef6d44c6f0a5edbe0c2adf86
jupiekim <jh.julie.kim@gmail.com>
2014-05-29 13:25:48 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,21 +1,21 @@
-public class Card{
+public class Card {
+    
     int value ; //1 will be $1M, 2 willbe $2M... money values can only be 1,2,3,4,5, and 10.
     String name;
     boolean inField;
     String type;
-
-    public Card(){ //will always be overridden
+    
+    public Card() { //will always be overridden
 	value = 1;
 	name = "";
 	inField = false;
-	
-}
-
-    public int getValue(){
+    }
+    
+    public int getValue() {
 	return value;
     }
-
-    public String getName(){
+    
+    public String getName() {
 	return name;
     }
     
