4b328d823f505a44dd538d8110a1247367a2b956
jupiekim <jh.julie.kim@gmail.com>
2014-05-29 13:21:08 -0400

    Update YellowPropertyCard.java

--- YellowPropertyCard.java
+++ YellowPropertyCard.java
@@ -1,6 +1,8 @@
 public class YellowPropertyCard{
-    public YellowPropertyCard(){
+	
+    public YellowPropertyCard() {
 	value = 3;
 	name =//not sure how to do naming;
 	    inField = false;
+}
 }
