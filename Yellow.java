45af49e064e36a960035989ee662ea624ecd7b92
jupiekim <jh.julie.kim@gmail.com>
2014-05-29 13:36:02 -0400

    Update Yellow.java

--- Yellow.java
+++ Yellow.java
@@ -1,10 +1,22 @@
-public class Yellow extends Card {
+public class Yellow extends Property {
 	
     public Yellow( String s ) {
 	value = 3;
 	name = s;
 	inField = false;
 	type = "Property";
+	rent1 = 2;
+	rent2 = 4; 
+	rent3 = 6;
+    }
+
+    public int getRent( int n ) {
+    	if (n == 1)
+	    return rent1;
+	if (n == 2) 
+	    return rent2;
+	else
+	    return rent3;
     }
 
 }
