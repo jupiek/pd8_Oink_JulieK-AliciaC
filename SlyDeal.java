8c688542c369a0a8f7728ef975b48418a7cd0405
aliciachen <mail4aliciac@gmail.com>
2014-06-02 13:29:20 -0400

    Delete SlyDeal.java

--- SlyDeal.java
+++
@@ -1,9 +0,0 @@
-public class SlyDeal extends ActionCard {
-    public ActionCard(){
-	description = "Steal a property from the player of your choice (cannot be part of a full set)";
-	value = 3;
-	name = "Sly Deal";
-	type = "Action";
-	inField = "false";
-    }
-}
