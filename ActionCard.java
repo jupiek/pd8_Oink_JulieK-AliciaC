9b2dfa66111fb8f23ea4900f233f6c2ec03e470d
jupiekim <jh.julie.kim@gmail.com>
2014-06-04 13:42:56 -0400

    Update ActionCard.java

--- ActionCard.java
+++ ActionCard.java
@@ -1,19 +1,16 @@
 public class ActionCard extends Card {
     
-    public ActionCard(String n) {
+    public ActionCard( String n ) {
 	type = "Action";
 	inField = false;
 	name = n;
-	if (n.equals ("Sly Deal")){
+	if (n.equals ("Sly Deal")) 
       	    value = 3;
-	}
-	else if (n.equals("Forced Deal")){
+	else if (n.equals("Forced Deal")) 
 	    value = 3;
-	}
-	else if (n.equals("It's my Birthday!")){	
+	else if (n.equals("It's my Birthday!")) 	
 	    value = 2;
-	}
-	else{ //remainging should be "Rent Blue" "Rent Yellow" cards etc.
+	else if (n.equals("Rent")) { //remaining should be "Rent Blue" "Rent Yellow" cards etc.
 	    value = 1;
 	}
     }
