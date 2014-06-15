76f6b8113fe30bdec7f7e4c0b6f33189605669bc
jupiekim <jh.julie.kim@gmail.com>
2014-06-15 00:44:29 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,35 +5,37 @@ public class Driver {
 
     public static void main( String[] args ) {
 
-	RQueue<Card> deck = new RQueue<Card>();
-	deck.enqueue( new PropertyCard("Pennsylvania Avenue", "Green"));
-	deck.enqueue( new PropertyCard("North Carolina Avenue", "Green"));
-	deck.enqueue( new PropertyCard("Pacific Avenue","Green"));
-	deck.enqueue( new PropertyCard("Atlantic Avenue" ,"Yellow"));
-	deck.enqueue( new PropertyCard("Marvin Gardens" ,"Yellow"));
-	deck.enqueue( new PropertyCard("Ventnor Avenue" ,"Yellow"));
-	deck.enqueue( new PropertyCard("Illinois Avenue","Red"));
-	deck.enqueue( new PropertyCard("Indiana Avenue","Red"));
-	deck.enqueue( new PropertyCard("Kentucky Avenue","Red"));
-	deck.enqueue( new PropertyCard("St. James Place","Orange"));
-	deck.enqueue( new PropertyCard("Tennessee Avenue","Orange"));
-	deck.enqueue( new PropertyCard("New York Avenue","Orange"));
-	deck.enqueue( new PropertyCard("St. Charles Place","Pink"));
-	deck.enqueue( new PropertyCard("Virginia Avenue","Pink"));
-	deck.enqueue( new PropertyCard("States Avenue","Pink"));
-
-
-	deck.enqueue( new ActionCard("Sly Deal"));
-	deck.enqueue( new ActionCard("Forced Deal"));
-	deck.enqueue( new ActionCard("It's my Birthday!"));
-	deck.enqueue(new RentCard("Yellow"));
-	deck.enqueue(new RentCard("Pink"));
-	deck.enqueue(new RentCard("Orange"));
-	deck.enqueue(new RentCard("Red"));
-	deck.enqueue(new RentCard("Green"));
-
-	deck.sample(); //shuffles deck
-
+	ArrayList<Card> cards = new ArrayList<Card>();
+	cards.add( new PropertyCard("Pennsylvania Avenue", "Green"));
+	cards.add( new PropertyCard("North Carolina Avenue", "Green"));
+	cards.add( new PropertyCard("Pacific Avenue","Green"));
+	cards.add( new PropertyCard("Atlantic Avenue" ,"Yellow"));
+	cards.add( new PropertyCard("Marvin Gardens" ,"Yellow"));
+	cards.add( new PropertyCard("Ventnor Avenue" ,"Yellow"));
+	cards.add( new PropertyCard("Illinois Avenue","Red"));
+	cards.add( new PropertyCard("Indiana Avenue","Red"));
+	cards.add( new PropertyCard("Kentucky Avenue","Red"));
+	cards.add( new PropertyCard("St. James Place","Orange"));
+	cards.add( new PropertyCard("Tennessee Avenue","Orange"));
+	cards.add( new PropertyCard("New York Avenue","Orange"));
+	cards.add( new PropertyCard("St. Charles Place","Pink"));
+	cards.add( new PropertyCard("Virginia Avenue","Pink"));
+	cards.add( new PropertyCard("States Avenue","Pink"));
+	cards.add( new ActionCard("Sly Deal"));
+	cards.add( new ActionCard("Forced Deal"));
+	cards.add( new ActionCard("It's my Birthday!"));
+	cards.add(new RentCard("Yellow"));
+	cards.add(new RentCard("Pink"));
+	cards.add(new RentCard("Orange"));
+	cards.add(new RentCard("Red"));
+	cards.add(new RentCard("Green"));
+	
+	ALStack<Card> deck = new ALStack<Card>();
+	for (int i = 0; i < cards.size(); i++) {
+	    int rand = (int) (cards.size() * Math.random());
+	    deck.push( cards.remove(rand) );
+	}
+	
 	CLList<Player> players = new CLList<Player>();
 	Player P1 = new Player("Me");
 	Player P2 = new Player("Julie");
@@ -44,7 +46,7 @@ public class Driver {
 	players.add(P3);
 	players.add(P4);
 	P1.turn(players, deck);
-
+	
     }
     
 }
