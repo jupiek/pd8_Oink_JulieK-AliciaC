2f885940308a880a23821e08e6d770120a135022
aliciachen <mail4aliciac@gmail.com>
2014-06-16 02:22:46 -0400

    more cards

--- Driver.java
+++ Driver.java
@@ -5,31 +5,53 @@ public class Driver {
 
     public static void main( String[] args ) {
 	ArrayList<Card> cards = new ArrayList<Card>();
-	for (int count = 0; count <2; count++) {
+	for (int count = 0; count <3; count++) {
 
-	cards.add( new PropertyCard("Pennsylvania Avenue", "Green"));
-	cards.add( new PropertyCard("North Carolina Avenue", "Green"));
-	cards.add( new PropertyCard("Pacific Avenue","Green"));
-	cards.add( new PropertyCard("Atlantic Avenue" ,"Yellow"));
-	cards.add( new PropertyCard("Marvin Gardens" ,"Yellow"));
-	cards.add( new PropertyCard("Ventnor Avenue" ,"Yellow"));
-	cards.add( new PropertyCard("Illinois Avenue","Red"));
-	cards.add( new PropertyCard("Indiana Avenue","Red"));
-	cards.add( new PropertyCard("Kentucky Avenue","Red"));
-	cards.add( new PropertyCard("St. James Place","Orange"));
-	cards.add( new PropertyCard("Tennessee Avenue","Orange"));
-	cards.add( new PropertyCard("New York Avenue","Orange"));
-	cards.add( new PropertyCard("St. Charles Place","Pink"));
-	cards.add( new PropertyCard("Virginia Avenue","Pink"));
-	cards.add( new PropertyCard("States Avenue","Pink"));
-	cards.add( new ActionCard("Sly Deal"));
-	cards.add( new ActionCard("Forced Deal"));
-	cards.add( new ActionCard("It's my Birthday!"));
-	cards.add(new RentCard("Yellow"));
-	cards.add(new RentCard("Pink"));
-	cards.add(new RentCard("Orange"));
-	cards.add(new RentCard("Red"));
-	cards.add(new RentCard("Green"));}
+	    cards.add( new PropertyCard("Pennsylvania Avenue", "Green"));
+	    cards.add( new PropertyCard("North Carolina Avenue", "Green"));
+	    cards.add( new PropertyCard("Pacific Avenue","Green"));
+	    cards.add( new PropertyCard("Atlantic Avenue" ,"Yellow"));
+	    cards.add( new PropertyCard("Marvin Gardens" ,"Yellow"));
+	    cards.add( new PropertyCard("Ventnor Avenue" ,"Yellow"));
+	    cards.add( new PropertyCard("Illinois Avenue","Red"));
+	    cards.add( new PropertyCard("Indiana Avenue","Red"));
+	    cards.add( new PropertyCard("Kentucky Avenue","Red"));
+	    cards.add( new PropertyCard("St. James Place","Orange"));
+	    cards.add( new PropertyCard("Tennessee Avenue","Orange"));
+	    cards.add( new PropertyCard("New York Avenue","Orange"));
+	    cards.add( new PropertyCard("St. Charles Place","Pink"));
+	    cards.add( new PropertyCard("Virginia Avenue","Pink"));
+	    cards.add( new PropertyCard("States Avenue","Pink"));
+	    cards.add( new ActionCard("Sly Deal"));
+	    //  cards.add( new ActionCard("Forced Deal"));
+	    cards.add( new ActionCard("It's my Birthday!"));
+	    cards.add(new RentCard("Yellow"));
+	    cards.add(new RentCard("Pink"));
+	    cards.add(new RentCard("Orange"));
+	    cards.add(new RentCard("Red"));
+	    cards.add(new RentCard("Green"));
+	    cards.add(new Card(1));
+	    cards.add(new Card(2));
+	    cards.add(new Card(3));
+	    cards.add(new Card(4));
+	    cards.add(new Card(5));
+	    cards.add(new Card(1));
+	    cards.add(new Card(2));
+	    cards.add(new Card(3));
+	    cards.add(new Card(4));
+	    cards.add(new Card(5));
+	    cards.add(new Card(1));
+	    cards.add(new Card(2));
+	    cards.add(new Card(3));
+	    cards.add(new Card(4));
+	    cards.add(new Card(5));
+	    cards.add(new Card(1));
+	    cards.add(new Card(2));
+	    cards.add(new Card(3));
+	    cards.add(new Card(4));
+	    cards.add(new Card(5));
+
+	}
 	
 	ALStack<Card> deck = new ALStack<Card>();
 	for (int i = 0; i < cards.size(); i++) {
